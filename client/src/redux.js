bb59bfc564cb0ef651f54a149a23e9b272f62ebb
Ruslan Kutynko <ruslan.kutynko@itechart-group.com>
2019-04-09 16:57:24 +0300

    added redux

---
+++ client/src/redux.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import { save, load } from "redux-localstorage-simple";
+import reducer from "./reducers";
+import saga from "./sagas";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = isProduction
+	? compose
+	: window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const history = createBrowserHistory();
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
+if (isProduction) middlewares.splice(0, 0, save());
+
+export default createStore(
+	combineReducers({
+		...reducer,
+		router: connectRouter(history)
+	}),
+	load(),
+	composeEnhancers(applyMiddleware(...middlewares))
+);
+
+sagaMiddleware.run(saga);
